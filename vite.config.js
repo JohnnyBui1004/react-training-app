3f32242ebc2d82713cb64a23a03d28f719b1dbe5
Tên Của Bạn <email@example.com>
2025-05-05 23:18:10 +0300

    change vite and package.json

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  //file and add the base property. The value is the name of your repository with leading and trailing slashes.
-  base: "/Personal-Trainer-React-App/",
+  base: "/",
   plugins: [react()],
 });
